48cec0722bd0df86546d845d9890d13513840fe2
eazimms <eric.allen.zimmerman@gmail.com>
2023-10-19 23:08:02 -0700

    Mysql connection working now, need to create db and functions for that, but otherwise good

--- server.js
+++ server.js
@@ -13,15 +13,15 @@ app.use(express.static("public"));
 //Sets up Express app to handle data parsing
 app.use(express.urlencoded({extended: true})); 
 
-// app.use(express.json); 
-// //Mysql stuff, will update later, need to create schema for this as well
-// var connection= mysql.createConnection({
-//   host: "localhost", 
-//   port: 3306, 
-//   user: "root", 
-//   password: "R0senrot!", 
-//   database: ""
-// }); 
+app.use(express.json); 
+//Mysql stuff, will update later, need to create schema for this as well
+const connection= mysql.createConnection({
+  host: "localhost", 
+  port: 3306, 
+  user: "root", 
+  password: "", 
+  database: "test"
+}); 
 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname+ "/index.html")); 
@@ -30,13 +30,13 @@ app.get("/", function(req, res) {
 
 
 
-//  connection.connect(function(err) {
-//    if(err) {
-//      console.error("error connecting: " + err.stack); 
-//      return; 
-//   }
-//    console.log("Connected as id: " + connection.threadId); 
-// }); 
+ connection.connect(function(err) {
+   if(err) {
+     console.error("error connecting: " + err.stack); 
+     return; 
+  }
+   console.log("Connected as id: " + connection.threadId); 
+}); 
 
 app.use('/', router); 
 
